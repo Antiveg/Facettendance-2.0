ea0bbbf268ca7bf989958d33855b54f33117858c
AntiVeggie <Antiveg@users.noreply.github.com>
2024-12-23 02:57:03 +0700

    Finishing

--- backend/models/UserPhotos.js
+++ backend/models/UserPhotos.js
@@ -1,32 +1,3 @@
-// const { Sequelize, DataTypes } = require('sequelize');
-// const sequelize = require('../db');
-
-// const UserPhotos = sequelize.define('UserPhotos', {
-//     userId: {
-//         type: DataTypes.INTEGER,
-//         allowNull: false,
-//         references: { model: 'Users', key: 'id' },
-//         primaryKey: true,
-//     },
-//     photo_url: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         primaryKey: true,
-//     },
-//     createdAt: {
-//         type: DataTypes.DATE,
-//         defaultValue: Sequelize.NOW,
-//     },
-// });
-
-// UserPhotos.associate = (models) => {
-//     UserPhotos.belongsTo(models.User, {
-//         foreignKey: 'userId',
-//     });
-// };
-
-// module.exports = UserPhotos;
-
 'use strict'
 const { Model, Sequelize } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
