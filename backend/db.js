2983a0680b4b453b3110629fe12447badb903a8a
AntiVeggie <Antiveg@users.noreply.github.com>
2024-12-23 11:09:48 +0700

    env control

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,16 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize')
 
-const config = require('./config/config.json');
-const sequelize = new Sequelize(config.development);
+const config = require('./config/config')
+const env = process.env.NODE_ENV || 'development'
+const dbConfig = config[env]
+
+const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect,
+})
 
 sequelize.authenticate()
     .then(() => console.log('Connection has been established successfully.'))
-    .catch(err => console.error('Unable to connect to the database:', err));
+    .catch(err => console.error('Unable to connect to the database:', err))
 
 module.exports = sequelize;
